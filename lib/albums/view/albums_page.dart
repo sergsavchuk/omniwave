857fc4a6f1ea309028108fafb40ef16d1fa67c6c
Sergey Savchuk <sleepingdog.dev@gmail.com>
2023-02-26 13:14:49 +0300

    adapt the AlbumsPage to small sceens

--- lib/albums/view/albums_page.dart
+++ lib/albums/view/albums_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:music_repository/music_repository.dart';
-
 import 'package:omniwave/albums/albums.dart';
 import 'package:omniwave/bloc/app_settings_bloc.dart';
-import 'package:omniwave/common/track_collection_card.dart';
 import 'package:omniwave/common/app_scaffold/app_scaffold.dart';
+import 'package:omniwave/common/track_collection_card.dart';
+import 'package:omniwave/utils.dart';
 
 class AlbumsPage extends StatelessWidget {
   const AlbumsPage({super.key});
@@ -41,8 +41,8 @@ class AlbumsView extends StatelessWidget {
     return BlocBuilder<AlbumsBloc, AlbumsState>(
       builder: (context, state) {
         return GridView.count(
-          crossAxisCount: 4,
-          childAspectRatio: 0.75,
+          crossAxisCount: Utils.isSmallScreen ? 2 : 4,
+          childAspectRatio: Utils.isSmallScreen ? 2 : 0.75,
           children: state.albums
               .map((album) => TrackCollectionCard(trackCollection: album))
               .toList(),
--- lib/common/track_collection_card.dart
+++ lib/common/track_collection_card.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:music_repository/music_repository.dart';
 import 'package:omniwave/common/player_controls/player_controls.dart';
 import 'package:omniwave/styles.dart';
+import 'package:omniwave/utils.dart';
 
 class TrackCollectionCard extends StatefulWidget {
   const TrackCollectionCard({
@@ -19,9 +20,6 @@ class TrackCollectionCard extends StatefulWidget {
 class _TrackCollectionCardState extends State<TrackCollectionCard> {
   bool _hovered = false;
 
-  String _trackCollectionImage(String? url) =>
-      widget.trackCollection.imageUrl ?? 'some-default-image-url';
-
   @override
   Widget build(BuildContext context) {
     return MouseRegion(
@@ -33,64 +31,147 @@ class _TrackCollectionCardState extends State<TrackCollectionCard> {
             .read<PlayerBloc>()
             .add(PlayerTrackCollectionPlayRequested(widget.trackCollection)),
         child: Card(
+          clipBehavior: Clip.antiAlias,
           color: Theme.of(context).colorScheme.primaryContainer,
           child: Padding(
-            padding: const EdgeInsets.all(Insets.small),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Flexible(
-                  child: Stack(
-                    children: [
-                      Center(
-                        child: AspectRatio(
-                          aspectRatio: 1,
-                          child: Image.network(
-                            _trackCollectionImage(
-                              widget.trackCollection.imageUrl,
-                            ),
-                            fit: BoxFit.fitHeight,
-                          ),
-                        ),
-                      ),
-                      if (_hovered) Container(color: Colors.white24),
-                      if (_hovered)
-                        Container(
-                          alignment: Alignment.center,
-                          child: const Icon(
-                            Icons.play_circle_fill,
-                            color: Colors.green,
-                            size: 50,
-                          ),
-                        ),
-                    ],
+            padding: Utils.isSmallScreen
+                ? EdgeInsets.zero
+                : const EdgeInsets.all(Insets.small),
+            child: Utils.isSmallScreen
+                ? _SmallCollectionInfo(
+                    collection: widget.trackCollection,
+                    hovered: _hovered,
+                  )
+                : _CollectionInfo(
+                    collection: widget.trackCollection,
+                    hovered: _hovered,
                   ),
-                ),
-                const SizedBox(
-                  height: Insets.small,
-                ),
-                Text(
-                  widget.trackCollection.name,
-                  softWrap: false,
-                  overflow: TextOverflow.ellipsis,
-                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                        fontWeight: FontWeight.w900,
-                        color: Theme.of(context).colorScheme.onPrimaryContainer,
-                      ),
-                ),
-                const SizedBox(
-                  height: Insets.small,
-                ),
-                Text(
-                  widget.trackCollection.artists.join(', '),
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
-            ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class _CollectionInfo extends StatelessWidget {
+  const _CollectionInfo({required this.collection, required this.hovered});
+
+  final TrackCollection collection;
+  final bool hovered;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Flexible(
+          child: _TrackCollectionCover(
+            imageUrl: collection.imageUrl ?? 'some-default-image-url',
+            hovered: hovered,
+          ),
+        ),
+        const SizedBox(
+          height: Insets.small,
+        ),
+        Text(
+          collection.name,
+          softWrap: false,
+          overflow: TextOverflow.ellipsis,
+          style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                fontWeight: FontWeight.w900,
+                color: Theme.of(context).colorScheme.onPrimaryContainer,
+              ),
+        ),
+        const SizedBox(
+          height: Insets.small,
+        ),
+        Text(
+          collection.artists.join(', '),
+          overflow: TextOverflow.ellipsis,
+        ),
+      ],
+    );
+  }
+}
+
+class _SmallCollectionInfo extends StatelessWidget {
+  const _SmallCollectionInfo({required this.collection, required this.hovered});
+
+  final TrackCollection collection;
+  final bool hovered;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        _TrackCollectionCover(
+          imageUrl: collection.imageUrl ?? 'some-default-image-url',
+          hovered: hovered,
+        ),
+        const SizedBox(
+          width: Insets.extraSmall,
+        ),
+        Flexible(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                collection.name,
+                softWrap: false,
+                overflow: TextOverflow.fade,
+                style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                      fontWeight: FontWeight.w900,
+                      color: Theme.of(context).colorScheme.onPrimaryContainer,
+                    ),
+              ),
+              Text(
+                collection.artists.join(', '),
+                softWrap: false,
+                overflow: TextOverflow.fade,
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          width: Insets.extraSmall,
+        ),
+      ],
+    );
+  }
+}
+
+class _TrackCollectionCover extends StatelessWidget {
+  const _TrackCollectionCover({required this.imageUrl, required this.hovered});
+
+  final String imageUrl;
+  final bool hovered;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Center(
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: Image.network(
+              imageUrl,
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+        ),
+        if (hovered) Container(color: Colors.white24),
+        if (hovered)
+          Container(
+            alignment: Alignment.center,
+            child: const Icon(
+              Icons.play_circle_fill,
+              color: Colors.green,
+              size: 50,
+            ),
+          ),
+      ],
     );
   }
 }
